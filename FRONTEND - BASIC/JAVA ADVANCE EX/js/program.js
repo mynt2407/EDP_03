b0130477e9ca9bab3a120c3aceccf55bf4c98487
mynt2407 <mynt2407@gmail.com>
2020-12-09 00:37:18 +0700

    abc

--- FRONTEND - BASIC/JAVA ADVANCE EX/js/program.js
+++ FRONTEND - BASIC/JAVA ADVANCE EX/js/program.js
@@ -3,3 +3,16 @@ $(function() {
     $(".main").load("main.html");
     $(".footer").load("footer.html");
 });
+
+function clickUsers() {
+    $(".main").load("body.html");
+}
+
+$(document).ready(function() {
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
